a23a64ebe33de2510dc57254205cec86c30a74ea
Koan Loc <loc.koan@yahoo.com>
2020-05-03 16:42:21 -0500

    added js functions

---
+++ script.js
@@ -0,0 +1,16 @@
+let beginButton = document.getElementById("begin-button")
+
+beginButton.addEventListener("click", beginGame)
+
+function beginGame() {
+    console.log("beginButton")
+    beginButton.classList.add("hide") /*confirms this function works */
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
